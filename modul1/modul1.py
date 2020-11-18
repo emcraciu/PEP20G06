59f0a54fa0ab7eb2223eb83a7625ec2b98136d58
Emanuel Craciun <emcraciu@cisco.com>
2020-11-18 21:34:06 +0200

    new

---
+++ modul1/modul1.py
@@ -0,0 +1,25 @@
+print('Hello World')
+print('Hello World', 'Hello World', 'Hello World', sep='- -')
+print('Hello World', end='$\n')
+
+hello = 'Hello World'
+print(hello)
+
+print(print)
+print()
+
+number1 = 1
+number2 = 2
+number3 = number1 + number2
+print(number3)
+
+number1 = 1
+number2 = '2'  # comment
+# number3 = number1 / number2
+print(number3)
+
+
+
+print(dir(number1))
+print(dir(number2))
+print(number1.__add__(2))
