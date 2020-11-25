5edc1713ee806d344e908e5c8b08101e9f67da88
Emanuel Craciun <emcraciu@cisco.com>
2020-11-25 23:01:50 +0200

    homework2

---
+++ modul2/homework2.py
@@ -0,0 +1,7 @@
+"""Homework 2 - needs to be presented before exam day"""
+
+# 1) Prove "and" operation takes precedence over "or" operation by setting
+# parentheses in (False or False and True or True)
+
+# 2) Get from input two diferent times in the format hh:mm:ss and print
+# the diference in seconds between them
--- modul2/modul2.py
+++ modul2/modul2.py
@@ -1,5 +1,6 @@
-# binary numbers
+### Integers
 
+# binary numbers
 # 0001 = 0b0001
 # 0002 = 0b0010
 # 0003 = 0b0011
@@ -7,7 +8,6 @@
 # 0005 = 0b0101
 
 # octal numbers
-
 # 0001 = 0o0001
 # 0002 = 0o0002
 # ...
@@ -16,7 +16,6 @@
 
 
 # hexadecimal numbers
-
 # 0001 = 0x0001
 # 0002 = 0x0002
 # ...
@@ -27,12 +26,17 @@
 # 0015 = 0o000f
 # 0016 = 0o0010
 
-# Print in base 2
-print(0b0100)
-print(5e100)
+# Print in different bases
+print('Base2 number (0b0100) is printed in Base10:', 0b0100)
+print('Base8 number (0o17) is printed in Base10:', 0o17)
+print('Base8 number (0x1f) is printed in Base10:', 0x1f)
+print()
+
+# Exponent uses base 10
+print('Number with exponent (5e10) is printed:', 5e10)
+print()
 
 # Arithmetic Operations
-
 number1 = 3
 number2 = 4
 number3 = -3
@@ -41,22 +45,28 @@ print('division 3/4:', number1 / number2)
 print('floordiv 4//3:', number2 // number1)
 print('remainder 4%3:', number2 % number1)
 print('negative 3:', -number1)
-print('zero:', number1 + number3)
+print('Result of 3 + (-3) is:', number1 + number3)
 print('3 to the power of 4:', number1 ** number2)
 print('Type of number -3:', type(number3))
+print()
 
+# Exercise use learned arithmetic operations
 a = 3
 b = 4
 c = 5
-
 result = (-b + pow((b ** 2) - 4 * a * c, 1 / 2)) / (2 * a)
-print(result)
+print('Result of quadratic formula is:', result)
+print()
 
+### Floats
 number4 = 0.75
 print('Type of number 0.75:', type(number4))
+print()
 
+### Complex
 number5 = 1.0 + 2.3j
 print('Type of 1.0+2.3j:', type(number5))
+print()
 
 # String quotes:
 string1 = 'Hello World'
@@ -64,6 +74,7 @@ string2 = "Hello World"
 string3 = '''Hello World'''
 string4 = """Hello World"""
 print(string1, string2, string3, string4)
+print()
 
 # String interpretation:
 string5 = u'Hello\nWorld'
@@ -72,44 +83,52 @@ string5 = r'Hello\nWorld'
 print('Row string:', string5)
 string5 = f'Hello World: {string5}'
 print('Formatted string:', string5)
+print()
 
 # String slices
-result = string5[4]
+result = string4[4]
 print('Forth char is:', result)
-result = string5[-3]
-print('third char from right is:', result)
-result = string5[4:9]
-print('Forth to seventh char is:', result)
-result = string5[9:4:-1]
-print('Forth to seventh char is:', result)
-result = string5[-2:-6:-1]
-print('result:', result)
-result = string5[-2:-7:-2]
-print('result:', result)
+result = string4[-3]
+print('Third char from right is:', result)
+result = string4[4:9]
+print('Forth to ninth char is:', result)
+result = string4[4:9:2]
+print('Forth to ninth char with step 2:', result)
+result = string4[9:4:-1]
+print('9 to 4 char with negative -1 step is:', result)
+result = string4[-2:-6:-1]
+print('-2 to -6 char with negative -1 step is:', result)
+result = string4[-2:-6:-2]
+print('-2 to -6 char with negative -1 step is:', result)
+print()
 
 # Dot notation for strings:
+string1 = 'Hello World: {}, {}'
 print('string actions:', dir(string1))
-print(string1.lower())
-print(string1.upper())
-print(string1.capitalize())
-print(string1.format())
+print('All lower case:', string1.lower())
+print('All upper case:', string1.upper())
+print('Upper first letter:', string1.capitalize())
+print('Formated string:', string1.format('one', 'two'))
+print('Index of first found "{":', string1.find('{'))
+print()
 
 # Dot notation for ints:
-print(number1.__add__(number2))
-print((3).__add__(4))
-print('multiplication:', number1.__mul__(number2))
-print('division:', number1.__truediv__(number2))
-print('power of:', number1.__pow__(number2), '==', number1 ** number2)
+print('int actions:', dir(number1))
+print('Numbers are:', number1, number2)
+print('Addition:', number1.__add__(number2))
+print('Use () to call methods on insta:', (3).__add__(4))
+print('Multiplication:', number1.__mul__(number2))
+print('Division:', number1.__truediv__(number2))
+print('Power, __pow__ is **:', number1.__pow__(number2), '==', number1 ** number2)
+print()
 
-
-
-# x = []
-# a = 0
-# for i in string1:
-#     if a%2:
-#         x.append(i.upper())
-#     else:
-#         x.append(i.lower())
-#     a = a + 1
-#
-# print(''.join(x))
+# Example
+result = ''
+a = 0
+for i in string1:
+    if a % 2:
+        result = result + i.upper()
+    else:
+        result = result + i.lower()
+    a = a + 1
+print('Lower and upper char sequence:', result)
